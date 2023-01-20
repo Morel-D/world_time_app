551682492ff223d409098697afcbd753745127f0
Morel-D <morelDen@gmail.com>
2023-01-20 11:21:58 -0800

    world Time Class

---
+++ lib/Services/world_time.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+
+class worldTime {
+
+  String location; // location of the user
+  late String time; // time
+  String path;
+  String flag;
+
+  worldTime({required this.location, required this.path, required this.flag });
+
+  Future<void> getData() async {
+
+    Uri url = Uri.parse('http://worldtimeapi.org/api/timezone/$path');
+
+    Response response = await get(url);
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // Setting time
+    String dataTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(dataTime);
+    //print(offset);
+
+    // creating dateTime object
+    DateTime now = DateTime.parse(dataTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set Time property
+
+    time = now.toString();
+
+
+  }
+}
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:world_time/Services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,38 +12,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  String time = "loading";
 
-  Uri url = Uri.parse("http://worldtimeapi.org/api/timezone/Africa/Douala");
-
-  Response response = await get(url);
-  Map data = jsonDecode(response.body);
-  //print(data);
-
-  // Setting time
-  String dataTime = data['datetime'];
-  String offset = data['utc_offset'].substring(1,3);
-  //print(dataTime);
-  //print(offset);
-
-  // creating dateTime object
-    DateTime now = DateTime.parse(dataTime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
+  void setWorldTime() async {
+    worldTime instance = worldTime(location: "douala", path: "africa/douala", flag: "cameroun.png");
+    await instance.getData();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
 
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
