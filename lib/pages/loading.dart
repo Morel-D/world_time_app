e1be23536dfd2bd8e9e592a6ae0e7106098aa879
Morel-D <morelDen@gmail.com>
2023-01-24 15:57:34 -0800

    Listview other countires

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -14,7 +14,7 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void setWorldTime() async {
-    worldTime instance = worldTime(location: "Douala", path: "africa/douala", flag: "cameroun.png");
+    worldTime instance = worldTime(location: "Yaounde", path: "africa/douala", flag: "cameroun.png");
     await instance.getData();
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location': instance.location,
--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/Services/world_time.dart';
 
 class Location extends StatefulWidget {
   const Location({Key? key}) : super(key: key);
@@ -9,6 +10,17 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
 
+  List locations = [
+    worldTime(path: 'Europe/London', location: 'London', flag: 'london.png'),
+    worldTime(path: 'Africa/Cario', location: 'Cario', flag: 'Cario.png'),
+    worldTime(path: 'Africa/Nairobi', location: 'Nairobi', flag: 'Nairobi.png'),
+    worldTime(path: 'America/Chicago', location: 'Chicago', flag: 'chicago.png'),
+    worldTime(path: 'America/New_York', location: 'New_York', flag: 'New_York.png'),
+    worldTime(path: 'Asia/Seoul', location: 'Seoul', flag: 'Seoul.png'),
+    worldTime(path: 'Asia/Jarkatar', location: 'Jarkatar', flag: 'Jarkatar.png'),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +29,22 @@ class _LocationState extends State<Location> {
         backgroundColor: Colors.lightBlueAccent,
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 2),
+              child: Card(
+                child: ListTile(
+                  onTap: (){
+                    print(locations[index].location);
+                  },
+                  title: Text(locations[index].location),
+                ),
+              ),
+            );
+          }
       ),
     );
   }
